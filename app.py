712d5f68cc9875ad7634486db6ae7ec705f46465
HACONGTUAn <86510911+HACONGTUAn@users.noreply.github.com>
2025-03-28 17:05:16 +0700

    Api
    Api tinh binh phuong

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/square', methods=['POST'])
+def calculate_square():
+    data = request.get_json()
+    number = data.get("number", 0)
+    result = number ** 2
+    return jsonify({"number": number, "square": result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
